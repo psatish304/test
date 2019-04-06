5e070b01cc837b59afc38311cf58dc117ed677e8
sai <sai@gmail.com>
2019-04-06 17:57:37 +0530

    Revert "age confirmation by user input as teenage or adult"
    This reverts commit 11f09eed07c2c3fc605a3fffb8b6efebd93afc98.

--- testingrepo.py
+++ testingrepo.py
@@ -3,9 +3,6 @@ age = int(input('Enter your age: '))
 if age > 20:
 	print('your are adult')
 
-elif age < 20 and age > 13:
-	print('you are teenage guy')
-
 else:
-    print('your are old..!!')
+	print('you are immature guy')
 
