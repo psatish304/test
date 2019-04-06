fb1f225783fc1081ff1ba9856edb41c69d4d2fb8
sai <sai@gmail.com>
2019-04-06 17:55:45 +0530

    Revert "new changes added"
    This reverts commit 4a2af0611db72be55599f97ca2e07b0d01a6d806.

--- testingrepo.py
+++ testingrepo.py
@@ -3,11 +3,9 @@ age = int(input('Enter your age: '))
 if age > 20:
 	print('your are adult')
 
-
 elif age < 20 and age > 13:
 	print('you are teenage guy')
 
 else:
     print('your are old..!!')
-    print('you will die soon..!!')
 
