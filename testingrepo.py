11f09eed07c2c3fc605a3fffb8b6efebd93afc98
sai <sai@gmail.com>
2019-04-06 17:17:35 +0530

    age confirmation by user input as teenage or adult

--- testingrepo.py
+++ testingrepo.py
@@ -3,6 +3,9 @@ age = int(input('Enter your age: '))
 if age > 20:
 	print('your are adult')
 
+elif age < 20 and age > 13:
+	print('you are teenage guy')
+
 else:
-	print('you are immature guy')
+    print('your are old..!!')
 
