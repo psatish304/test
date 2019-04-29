474e1066085238aae8986cccafc1f26d6218f40c
sai <sai@gmail.com>
2019-04-29 15:18:44 +0530

    Water

--- testingrepo.py
+++ testingrepo.py
@@ -1,8 +1,23 @@
-age = int(input('Enter your age: '))
+'''class computer:
+    def __init__(self,cpu,ram):
+        self.cpu=cpu
+        self.ram=ram
 
-if age > 20:
-	print('your are adult')
+    def config(self):
+        print('config is',self.cpu,self.ram)
 
+
+com1=computer()
+com2=computer()
+
+
+com1.config('i5','8gb')
+com2.config('Rayzen 3',8)'''
+
+hungry = input('are you hungry/thirsty.?').lower()
+if hungry == 'hungry':
+	print('Please Eat something which you can')
+elif hungry=='thirsty':
+	print('please drink something..')
 else:
-	print('you are immature guy')
-
+	print('Enjoy and continue your work')
